70086dfc8e700d36040d7e3c7c4f1ca81161972b
Pujasaha151 <ps.pujasaha.bd@gamil.com>
2025-04-15 15:46:27 +0600

    3rd commit

--- area.cpp
+++ area.cpp
@@ -8,3 +8,29 @@ public:
     virtual double area() const = 0;
     virtual ~Shape() = default;
 };
+class Circle : public Shape {
+    double radius;
+public:
+    Circle(double r) : radius(r) {}
+    double area() const override {
+        return M_PI * radius * radius;
+    }
+};
+
+class Square : public Shape {
+    double side;
+public:
+    Square(double s) : side(s) {}
+    double area() const override {
+        return side * side;
+    }
+};
+
+class Rectangle : public Shape {
+    double width, height;
+public:
+    Rectangle(double w, double h) : width(w), height(h) {}
+    double area() const override {
+        return width * height;
+    }
+};
